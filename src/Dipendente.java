19b418ea7beb4356c3fba6537a5913dcc4f50b86
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-12-20 09:16:22 +0100

    Attributi, costruttori e metodi get e set della classe Dipendente.

--- src/Dipendente.java
+++ src/Dipendente.java
@@ -1,3 +1,24 @@
 public class Dipendente {
-
+    private String nome;
+    private double stipendio;
+    public Dipendente(){
+        nome="";
+        stipendio=0;
+    }
+    public Dipendente(String nome,double stipendio){
+        this.nome=nome;
+        this.stipendio=stipendio;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setStipendio(double stipendio){
+        this.stipendio=stipendio;
+    }
+    public double getStipendio(){
+        return stipendio;
+    }
 }
